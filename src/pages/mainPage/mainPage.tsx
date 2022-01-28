54f2be9b27929a838929c7bc6f4e92b2f3db8106
Anders <anders.morille@gmail.com>
2022-01-28 21:27:24 +0100

    Cleaned mainpage

--- src/pages/mainPage/mainPage.tsx
+++ src/pages/mainPage/mainPage.tsx
@@ -1,25 +1,16 @@
-import { useContext, useEffect, useState } from 'react';
-import { UserContext } from '../../App';
+import { useEffect, useState } from 'react';
 import JobCard from '../../components/jobCard/JobCard';
 import myFireBase, { listenForJobChanges } from '../../utils/myFireBase';
 import { IFullJob } from '../../utils/types';
 import './style.css';
 
 export default function MainPage() {
-	const userContext = useContext(UserContext);
 	const [jobs, setJobs] = useState<IFullJob[]>([]);
 
 	useEffect(
-		() =>
-			listenForJobChanges(() => {
-				myFireBase.jobs.getJobs().then((jobs) => {
-					setJobs(jobs);
-				});
-			}),
+		() => listenForJobChanges(() => myFireBase.jobs.getJobs().then(setJobs)),
 		[]
 	);
-
-	const takeJob = (id: string) => {};
 
 	return (
 		<div className="card-container">
